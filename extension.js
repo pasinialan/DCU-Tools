6a30816ba2c99e8066adda0471b5cb3e40d81cd6
Alan Pasini <alan.pasini@compasso.com.br>
2020-11-03 19:25:12 -0300

    logs add and directory check

--- extension.js
+++ extension.js
@@ -2,21 +2,42 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const cp = require('child_process');
+const fs = require("fs");
+
 const APP_KEY = vscode.workspace.getConfiguration('dcu-tools').appKey;
 const ENVIRONMENT = vscode.workspace.getConfiguration('dcu-tools').environment;
+let logger = vscode.window.createOutputChannel("DCU-Tools");
+
+const checkDir = (path) => {
+    try {
+        if (fs.existsSync(path)) {
+            logger.appendLine("[formatPath][widgetPath][Directory exists.]");
+            return true;
+        } else {
+            logger.appendLine("[formatPath][widgetPath][Directory does not exist.]");
+            return false;
+        }
+    } catch(e) {
+        logger.appendLine("[formatPath][widgetPath][An error occurred.]");
+        return false;
+    }
+};
 
 const formatPath = (widgetPath) => {
     if (widgetPath.startsWith("/")) {
         widgetPath = widgetPath.replace("/", "");
     }
+    logger.appendLine(`[formatPath][widgetPath][${widgetPath}]`);
     return widgetPath;
 };
 
 const validateDCU = () => 
     new Promise ((resolve, reject) => {
         executeShell("dcu --version").then((data) => {
+            logger.appendLine("[validateDCU][resolve]");
             return resolve(true);
         }, (err) => {
+            logger.appendLine("[validateDCU][reject]");
             return reject(false);
         });
     });
@@ -31,13 +52,16 @@ const checkParams = (...params) => {
 
 const executeShell = (cmd, rootDir) =>
     new Promise ((resolve, reject) => {
+        logger.appendLine(`[executeShell][rootDir][${rootDir}]`);
         cp.exec("set NODE_TLS_REJECT_UNAUTHORIZED=0");
         cp.exec(cmd, {
             cwd: rootDir
         }, (err, out) => {
+            logger.appendLine(`[executeShell][err][${err}]`);
             if (err)
                 return reject(err);
 
+            logger.appendLine(`[executeShell][out][${out}]`);
             return resolve(out);
         });
         
@@ -74,106 +98,88 @@ function activate(context) {
     });
 
     let getAllWidgets = vscode.commands.registerCommand('dcu-tools.getAllWidgets', function (uri) {
-        
-        validateDCU().then(data => {
-            let folderPath = vscode.workspace.rootPath; // get the open folder path
-            let widgetPath = formatPath(folderPath);
-            widgetPath = `${widgetPath}\\widget`;
+        let folderPath = vscode.workspace.rootPath; // get the open folder path
+        let widgetPath = formatPath(folderPath);
+        widgetPath = `${widgetPath}\\widget`;
 
-            executeShell('dcu --refresh "' + widgetPath + '" ' + ENVIRONMENT + ' --applicationKey ' + APP_KEY, folderPath).then((cmd1) => {
-                vscode.window.showInformationMessage('Get all widgets with success!');
-            }, function(err) {
-                vscode.window.showErrorMessage('Error On Get all widgets!', err);
-            });		
-        }, (err) => {
-            console.error('[data err]', err);
-            vscode.window.showErrorMessage("DCU error, please try to reinstall your DCU extension.");
+        if (!checkDir(widgetPath)) {
+            vscode.window.showErrorMessage(`Can't find directory ${widgetPath}, check it.`);
+            return;
+        }
+
+        executeShell('dcu --refresh "' + widgetPath + '" ' + ENVIRONMENT + ' --applicationKey ' + APP_KEY, folderPath).then((cmd1) => {
+            vscode.window.showInformationMessage('Get all widgets with success!');
+        }, function(err) {
+            vscode.window.showErrorMessage(`Error On Get all widgets! ${err}`);
         });
     });
 
 	let refreshWidget = vscode.commands.registerCommand('dcu-tools.refreshWidget', function (uri) {
-        
-        validateDCU().then(data => {
-            let folderPath = vscode.workspace.rootPath; // get the open folder path
-            let widgetPath = formatPath(uri.path);
+        let folderPath = vscode.workspace.rootPath; // get the open folder path
+        let widgetPath = formatPath(uri.path);
 
-            executeShell('dcu --refresh "' + widgetPath + '" ' + ENVIRONMENT + ' --applicationKey ' + APP_KEY, folderPath).then((cmd1) => {
-                vscode.window.showInformationMessage('Refresh Widget success!');
-            }, function(err) {
-                console.error('[data err]', err);
-                vscode.window.showErrorMessage('Error On Refresh Widget. Try to delete all files inside the widget folder and rerun a refresh widget script.');
-            });		
-        }, (err) => {
+        executeShell('dcu --refresh "' + widgetPath + '" ' + ENVIRONMENT + ' --applicationKey ' + APP_KEY, folderPath).then((cmd1) => {
+            vscode.window.showInformationMessage('Refresh Widget success!');
+        }, function(err) {
             console.error('[data err]', err);
-            vscode.window.showErrorMessage("DCU error, please try to reinstall your DCU extension.");
+            vscode.window.showErrorMessage(`Error On Refresh Widget. Try to delete all files inside the widget folder and rerun a refresh widget script. ${err}`);
         });
     });
 
     let getGlobals = vscode.commands.registerCommand('dcu-tools.getGlobals', function (uri) {
-        
-        validateDCU().then(data => {
-            let folderPath = vscode.workspace.rootPath; // get the open folder path
-            let widgetPath = formatPath(folderPath);
-            widgetPath = `${widgetPath}\\global`;
+        let folderPath = vscode.workspace.rootPath; // get the open folder path
+        let widgetPath = formatPath(folderPath);
+        widgetPath = `${widgetPath}\\global`;
 
-            executeShell('dcu --refresh "' + widgetPath + '" ' + ENVIRONMENT + ' --applicationKey ' + APP_KEY, folderPath).then((cmd1) => {
-                vscode.window.showInformationMessage('Refresh globals with success!');
-            }, function(err) {
-                vscode.window.showErrorMessage('Error On Refresh globals!', err);
-            });		
-        }, (err) => {
-            console.error('[data err]', err);
-            vscode.window.showErrorMessage("DCU error, please try to reinstall your DCU extension.");
+        if (!checkDir(widgetPath)) {
+            vscode.window.showErrorMessage(`Can't find directory ${widgetPath}, check it.`);
+            return;
+        }
+
+        executeShell('dcu --refresh "' + widgetPath + '" ' + ENVIRONMENT + ' --applicationKey ' + APP_KEY, folderPath).then((cmd1) => {
+            vscode.window.showInformationMessage('Refresh globals with success!');
+        }, function(err) {
+            vscode.window.showErrorMessage(`Error On Refresh globals! ${err}`);
         });
     });
 
     let getTheme = vscode.commands.registerCommand('dcu-tools.getTheme', function (uri) {
         
-        validateDCU().then(data => {
-            let folderPath = vscode.workspace.rootPath; // get the open folder path
-            let widgetPath = formatPath(folderPath);
-            widgetPath = `${widgetPath}\\theme`;
+        let folderPath = vscode.workspace.rootPath; // get the open folder path
+        let widgetPath = formatPath(folderPath);
+        widgetPath = `${widgetPath}\\theme`;
 
-            executeShell('dcu --refresh "' + widgetPath + '" ' + ENVIRONMENT + ' --applicationKey ' + APP_KEY, folderPath).then((cmd1) => {
-                vscode.window.showInformationMessage('Refresh themes with success!');
-            }, function(err) {
-                vscode.window.showErrorMessage('Error On Refresh themes!', err);
-            });		
-        }, (err) => {
-            console.error('[data err]', err);
-            vscode.window.showErrorMessage("DCU error, please try to reinstall your DCU extension.");
-        });
+        if (!checkDir(widgetPath)) {
+            vscode.window.showErrorMessage(`Can't find directory ${widgetPath}, check it.`);
+            return;
+        }
+
+        executeShell('dcu --refresh "' + widgetPath + '" ' + ENVIRONMENT + ' --applicationKey ' + APP_KEY, folderPath).then((cmd1) => {
+            vscode.window.showInformationMessage('Refresh themes with success!');
+        }, function(err) {
+            vscode.window.showErrorMessage(`Error On Refresh themes! \n ${err}`);
+        });		
     });
 
     let updatePath = vscode.commands.registerCommand('dcu-tools.updatePath', function (uri) {
-		validateDCU().then(data => {
-            let folderPath = vscode.workspace.rootPath; // get the open folder path
-            let widgetPath = formatPath(uri.path);
+        let folderPath = vscode.workspace.rootPath; // get the open folder path
+        let widgetPath = formatPath(uri.path);
 
-            executeShell('dcu --putAll "' + widgetPath + '" --applicationKey ' + APP_KEY, folderPath).then((cmd1) => {
-                vscode.window.showInformationMessage('Update File success!');
-            }, function(err) {
-                vscode.window.showErrorMessage('Error On Update File!', err);
-            });	
-        }, (err) => {
-            console.error('[data err]', err);
-            vscode.window.showErrorMessage("DCU error, please try to reinstall your DCU extension.");
+        executeShell('dcu --putAll "' + widgetPath + '" --applicationKey ' + APP_KEY, folderPath).then((cmd1) => {
+            vscode.window.showInformationMessage('Update File success!');
+        }, function(err) {
+            vscode.window.showErrorMessage(`Error On Update File! ${err}`);
         });
 	});
     
     let updateFile = vscode.commands.registerCommand('dcu-tools.updateFile', function (uri) {
-		validateDCU().then(data => {
-            let folderPath = vscode.workspace.rootPath; // get the open folder path
-            let widgetPath = formatPath(uri.path);
+        let folderPath = vscode.workspace.rootPath; // get the open folder path
+        let widgetPath = formatPath(uri.path);
 
-            executeShell('dcu --put "' + widgetPath + '" --applicationKey ' + APP_KEY, folderPath).then((cmd1) => {
-                vscode.window.showInformationMessage('Update File success!');
-            }, function(err) {
-                vscode.window.showErrorMessage('Error On Update File!', err);
-            });	
-        }, (err) => {
-            console.error('[data err]', err);
-            vscode.window.showErrorMessage("DCU error, please try to reinstall your DCU extension.");
+        executeShell('dcu --put "' + widgetPath + '" --applicationKey ' + APP_KEY, folderPath).then((cmd1) => {
+            vscode.window.showInformationMessage('Update File success!');
+        }, function(err) {
+            vscode.window.showErrorMessage(`Error On Update File! ${err}`);
         });
 	});
 
